2f674e2a01f64f37ee41c541ea2502e3efac75bd
Your Correct GitHub Username <alieelpozo3@gmail.com>
2025-07-29 23:22:47 -0400

    fix: increased mobile button height to 160px for better text display on iPhone

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2628077425"
+    serviceWorkerVersion: "1865742507"
   }
 });
